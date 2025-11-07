ac504407466ca249011dca8db17189752018094a
Halmand <56235457+Halmand@users.noreply.github.com>
2025-11-07 23:17:53 +0100

    Create words.js

---
+++ admin/words.js
@@ -0,0 +1,19 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+  "Einstellungen": { "en": "Settings", "de": "Einstellungen" },
+  "Phone ID": { "en": "Phone ID", "de": "Phone ID" },
+  "Abfrageintervall (Sekunden)": {
+    "en": "Polling interval (seconds)",
+    "de": "Abfrageintervall (Sekunden)"
+  },
+  "Batteriestatus anzeigen": {
+    "en": "Show battery status",
+    "de": "Batteriestatus anzeigen"
+  },
+  "Zeitstempel anzeigen": {
+    "en": "Show timestamp",
+    "de": "Zeitstempel anzeigen"
+  }
+};
